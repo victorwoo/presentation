55d3073dc102be0d27cee04121de7e8ac30437fa
wub <victorwoo@gmail.com>
2013-08-10 13:30:51 +0800

    Create main page.

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -1,1 +1,16 @@
+$(function(){
+    $("#content table#main a").click(function() {
+        var hash = this.hash.slice(1);
+        var url="";
+        switch (hash) {
+            case "01":
+                url = "pages/01/01.html";
+                break;
+            case "02":
+                url = "http://www.google.com.hk";
+                break;
+        }
 
+        $("#content").load(url);
+    });
+})
