f2c5cd60316adb5693d6360e213228713b6f7ba6
wub <victorwoo@gmail.com>
2013-08-10 22:51:54 +0800

    Complete first version.

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -1,6 +1,11 @@
-$(function(){
+$(function () {
+    $.ajaxSetup({
+        // Disable caching of AJAX responses
+        cache: false
+    });
+
     $('.marquee').marquee({
-        speed: 10000,
+        speed: 11000,
         gap: 20,
         delayBeforeStart: 0,
         direction: 'left',
@@ -8,20 +13,59 @@ $(function(){
         pauseOnHover: true
     });
 
+    function changeBackground(backgroundClass) {
+        $("#content").empty();
+        $(document.body).removeClass();
+        $(document.body).addClass(backgroundClass);
+    }
 
+    function loadMain() {
+        changeBackground("body-default");
 
-    $("#content table#main a").click(function() {
-        var hash = this.hash.slice(1);
-        var url="";
-        switch (hash) {
-            case "01":
-                url = "pages/01/01.html";
-                break;
-            case "02":
-                url = "http://www.google.com.hk";
-                break;
-        }
+        $("#content").load("pages/main.html", function () {
+            $("#content table#main a").click(function () {
+                var hash = this.hash.slice(1);
+                switch (hash) {
+                    case "01":
+                        changeBackground("body-sub01");
+                        $("#content").load("pages/01.html", function () {
+                            $(".button.back").click(function () {
+                                loadMain();
+                            });
+                        });
+                        break;
+                    case "02":
+                        changeBackground("body-sub02");
+                        $("#content").load("pages/02.html", function () {
+                            $(".buttons").click(function () {
+                                loadMain();
+                            });
+                        });
+                        break;
+                    case "03":
+                        changeBackground("body-sub03");
+                        $("#content").load("pages/03.html", function () {
+                            $(".buttons").click(function () {
+                                loadMain();
+                            });
+                        });
+                        break;
+                    case "04":
+                        changeBackground("body-sub04");
+                        $("#content").load("pages/04.html", function () {
+                            $(".buttons").click(function () {
+                                loadMain();
+                            });
+                        });
+                        break;
+                }
+            });
+        });
+    }
 
-        $("#content").load(url);
+    $('.button-back').click(function () {
+        loadMain();
     });
+
+    loadMain();
 })
